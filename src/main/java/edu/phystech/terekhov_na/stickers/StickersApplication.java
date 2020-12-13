d7e8043fc366a7e5d2510ff57cf7667ac99b3539
Nikolay Terekhov <terekhov.na@phystech.edu>
2020-12-13 12:41:23 +0300

    i commit

--- src/main/java/edu/phystech/terekhov_na/stickers/StickersApplication.java
+++ src/main/java/edu/phystech/terekhov_na/stickers/StickersApplication.java
@@ -24,7 +24,7 @@ public class StickersApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "http://178.154.235.120:3000/")
+                        .allowedOrigins("http://localhost:3000", "http://178.154.235.120:3000")
                         .allowCredentials(true);
             }
         };
